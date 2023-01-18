34a355a53da300e261ab92d99c57dad03721b669
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-01-18 10:37:30 +0800

    Update crawl_ranking.py

--- crawl_ranking.py
+++ crawl_ranking.py
@@ -13,7 +13,7 @@ for i in tqdm(range(13878)):
     )
     # try to catch no reponse (retry 3 times)
     retry = 0
-    while graphql_ranking.status_code != 200 and retry < 4:
+    while graphql_ranking.status_code != 200 and retry < 3:
         print(f'page {str(i+1)} retry {retry+1} times')
         time.sleep(60)
         graphql_ranking = requests.post(
@@ -21,7 +21,7 @@ for i in tqdm(range(13878)):
             json = {"query": "{\n  globalRanking(page: "+str(i+1)+") {\n    totalUsers\n    userPerPage\n    myRank {\n      ranking\n      currentGlobalRanking\n      currentRating\n      dataRegion\n      user {\n        nameColor\n        activeBadge {\n          displayName\n          icon\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    rankingNodes {\n      ranking\n      currentRating\n      currentGlobalRanking\n      dataRegion\n      user {\n        username\n        nameColor\n        activeBadge {\n          displayName\n          icon\n          __typename\n        }\n        profile {\n          userAvatar\n          countryCode\n          countryName\n          realName\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"}
         )
         retry += 1
-    if retry == 4:
+    if retry == 3:
         print(f'skip page {str(i+1)}')
         continue
 
@@ -36,19 +36,39 @@ for i in tqdm(range(13878)):
                     "variables": {"username": rankingNode['user']['username']}
                 }
             )
+            # try to catch no reponse (retry 3 times)
+            retry = 0
+            while graphql_user.status_code != 200 and retry < 3:
+                print(f'user {rankingNode["user"]["username"]} retry {retry+1} times')
+                time.sleep(60)
+                graphql_user = requests.post(
+                    'https://leetcode.com/graphql/',
+                    json = {
+                        "query": "query userPublicProfile($username: String!) {\n  matchedUser(username: $username) {\n    contestBadge {\n      name\n      expired\n      hoverText\n      icon\n    }\n    username\n    githubUrl\n    twitterUrl\n    linkedinUrl\n    profile {\n      ranking\n      userAvatar\n      realName\n      aboutMe\n      school\n      websites\n      countryName\n      company\n      jobTitle\n      skillTags\n      postViewCount\n      postViewCountDiff\n      reputation\n      reputationDiff\n      solutionCount\n      solutionCountDiff\n      categoryDiscussCount\n      categoryDiscussCountDiff\n    }\n  }\n}\n    ",
+                        "variables": {"username": rankingNode['user']['username']}
+                    }
+                )
+                retry += 1
+            if retry == 3:
+                print(f'user {rankingNode["user"]["username"]} query failed')
             
+
             currentRating.append(rankingNode['currentRating'])
             currentGlobalRanking.append(rankingNode['currentGlobalRanking'])
             userAvatar.append(rankingNode['user']['profile']['userAvatar'])
             username.append(rankingNode['user']['username'])
             realName.append(rankingNode['user']['profile']['realName'])
             ranking.append(
-                # handle 'That user does not exist.'
-                graphql_user.json()['data']['matchedUser']['profile']['ranking'] if graphql_user.json()['data']['matchedUser'] else None
+                graphql_user.json()['data']['matchedUser']['profile']['ranking'] if (
+                    graphql_user.status_code == 200 and graphql_user.json()['data']['matchedUser'] # handle 'That user does not exist.'
+                ) else None
             )
             school.append(
-                graphql_user.json()['data']['matchedUser']['profile']['school'] if graphql_user.json()['data']['matchedUser'] else None
+                graphql_user.json()['data']['matchedUser']['profile']['school'] if (
+                    graphql_user.status_code == 200 and graphql_user.json()['data']['matchedUser'] # handle 'That user does not exist.'
+                ) else None
             )
+
 
 ranking_DF['currentRating'] = currentRating
 ranking_DF['currentGlobalRanking'] = currentGlobalRanking
@@ -57,6 +77,7 @@ ranking_DF['username'] = username
 ranking_DF['realName'] = realName
 ranking_DF['ranking'] = ranking
 ranking_DF['school'] = school
+
 
 client = pymongo.MongoClient( os.environ['MONGODB_URI'] )
 db = client['LC-TW-Board']
